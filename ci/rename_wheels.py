4d02f7daf9aca5d485e93d0c6e474419e6ed5855
Aki Ariga <chezou+github@gmail.com>
2025-11-23 17:40:54 -0800

    Update wheel name

--- ci/rename_wheels.py
+++ ci/rename_wheels.py
@@ -6,12 +6,12 @@ import shutil
 if __name__ == "__main__":
     # auditwheel will try to vendor cublas/cudart/curand etc - which makes the
     # package unacceptably large. instead of running auditwheel lets just rename
-    # to be manylinux
+    # to be manylinux_2_28 (AlmaLinux 8 based)
     wheelhouse = os.path.join(
         os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "wheelhouse"
     )
     for filename in os.listdir(wheelhouse):
         if filename.endswith(".whl") and "linux" in filename and "manylinux" not in filename:
-            new_filename = filename.replace("linux", "manylinux2014")
+            new_filename = filename.replace("linux", "manylinux_2_28")
             print("moving '%s' to '%s'" % (filename, new_filename))
             shutil.move(os.path.join(wheelhouse, filename), os.path.join(wheelhouse, new_filename))
