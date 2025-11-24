4f12959978ed05cd66c4ccd7cb43e233f224cb82
Aki Ariga <chezou+github@gmail.com>
2025-11-23 17:18:04 -0800

    Build with CUDA 12.9

--- ci/install_cuda.sh
+++ ci/install_cuda.sh
@@ -1,9 +1,9 @@
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
 yum install --setopt=obsoletes=0 -y \
-    cuda-nvcc-12-6 \
-    cuda-cudart-devel-12-6 \
-    libcurand-devel-12-6 \
-    libcublas-devel-12-6 \
-    cuda-nvprof-12-6 \
+    cuda-nvcc-12-9 \
+    cuda-cudart-devel-12-9 \
+    libcurand-devel-12-9 \
+    libcublas-devel-12-9 \
+    cuda-nvprof-12-9 \
     ninja-build
-ln -s cuda-12.6 /usr/local/cuda
+ln -s cuda-12.9 /usr/local/cuda
